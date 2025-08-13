d725d4d8522fbe2cf99fd5c23d60cffa48f337cd
joses <josesamuelsantos321@hotmail.com>
2025-08-13 14:48:15 -0300

    estruturacao do form page1

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,29 @@ export function App() {//usa o nome da classe
             <Container>
                 <CountDown />
             </Container>
+
+            <Container>
+                <form className='form'>{/*É o container principal de um formulário.Agrupa campos de entrada, botões e outros elementos.*/}
+                    <div className='formRow'>
+                        {/*htmlFor e id tem que ser iguais*/}
+                        <label htmlFor='meuInput'>task</label>{/*Serve para identificar um campo de entrada.*/}
+                        <input id='meuInput' type='text' />{/*É o campo de entrada*/}
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>                    
+                </form>
+            </Container>
         </>
     )
 }
